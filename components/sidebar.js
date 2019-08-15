f2e982c2f1f53e1b0da75a79337add04f74a40dd
Evan Tahler <evan.tahler@voom.flights>
2019-08-15 12:35:30 -0700

    triangles

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import Styles from './styles/variables.json'
 import { Image } from 'react-bootstrap'
+import Trianglify from 'trianglify'
+
+let renderCount = 0
+const maxRenders = 100
+
+function renderTriangles () {
+  if (process.browser & renderCount < maxRenders) {
+    const sidebar = document.getElementById('sidebar')
+    const dimensions = sidebar.getClientRects()[0]
+    const pattern = Trianglify({
+      width: dimensions.width,
+      height: dimensions.height * 2,
+      x_colors: 'YlOrRd',
+      cell_size: 30
+    })
+    // sidebar.appendChild(pattern.canvas())
+    sidebar.style['background-image'] = `url(${pattern.png()})`
+    renderCount++
+  }
+}
 
 function Sidebar ({ title, content }) {
   const SidebarStyle = {
     backgroundColor: Styles.$orange,
-    minHeight: '100vh',
     height: '100%',
-    width: '100%',
     textAlign: 'center',
     paddingTop: 100
   }
@@ -18,8 +36,11 @@ function Sidebar ({ title, content }) {
     margin: 20
   }
 
+  setTimeout(renderTriangles, 1)
+  setInterval(renderTriangles, 1000 * 5)
+
   return (
-    <div className='sidebar' style={SidebarStyle}>
+    <div className='sidebar' id='sidebar' style={SidebarStyle}>
       <Image src='static/images/evan.jpg' className='rounded-circle' style={{ maxWidth: '80%' }} />
 
       <div style={{
