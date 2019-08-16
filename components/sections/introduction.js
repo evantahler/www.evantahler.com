89665e5c1a7e4298dffd6b2b46ea71c61aeca932
Evan Tahler <evan.tahler@voom.flights>
2019-08-16 09:44:50 -0700

    fix style

--- components/sections/introduction.js
+++ components/sections/introduction.js
@@ -15,17 +15,17 @@ function Introduction () {
         <h4>I use my <BoldWords text='Product Management' />, <BoldWords text='Software Engineering' />, and <BoldWords text='Leadership' /> skills to help build a team capable of creating world-class Digital Consumer Products.</h4>
       </Jumbotron>
 
-        <Row>
-          <Col md={4} style={{ textAlign: 'center' }}>
-            <a href='https://twitter.com/evantahler'><BigGlyf name='twitter' /><br /> @evantahler</a>
-          </Col>
-          <Col md={4} style={{ textAlign: 'center' }}>
-            <a href='https://linkedin.com/in/evantahler'><BigGlyf name='linkedin' /><br /> @evantahler</a>
-          </Col>
-          <Col md={4} style={{ textAlign: 'center' }}>
-            <a href='mailto:evan@evantahler.com'><BigGlyf name='inbox' /><br />evan@evantahler.com</a>
-          </Col>
-        </Row>
+      <Row>
+        <Col md={4} style={{ textAlign: 'center' }}>
+          <a href='https://twitter.com/evantahler'><BigGlyf name='twitter' /><br /> @evantahler</a>
+        </Col>
+        <Col md={4} style={{ textAlign: 'center' }}>
+          <a href='https://linkedin.com/in/evantahler'><BigGlyf name='linkedin' /><br /> @evantahler</a>
+        </Col>
+        <Col md={4} style={{ textAlign: 'center' }}>
+          <a href='mailto:evan@evantahler.com'><BigGlyf name='inbox' /><br />evan@evantahler.com</a>
+        </Col>
+      </Row>
     </>
   )
 }
