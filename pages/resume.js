2601c286dc107f087a54cf0242be3ec7cc311b5e
Evan Tahler <evan@evantahler.com>
2019-09-09 19:10:28 -0700

    company links open in new page

--- pages/resume.js
+++ pages/resume.js
@@ -24,11 +24,11 @@ function ResumePage () {
         </Alert>
 
         <p>
-          Evan is looking for what's next! Most recently, he was the Chief Product Officer at <a href='https://www.voom.flights'>Voom, an Airbus Company</a>, providing an urban air mobility to cities around the world.
+          Evan is looking for what's next! Most recently, he was the Chief Product Officer at <a target='_new' href='https://www.voom.flights'>Voom, an Airbus Company</a>, providing an urban air mobility to cities around the world.
         </p>
 
         <p>
-          Evan has deep expertise in building the technical side of B2C products as well as the teams required to do so. He’s helped companies like <a href='https://www.disney.com'>Disney</a>, <a href='https://www.taskrabbit.com'>TaskRabbit</a>, <a href='https://www.modcloth.com'>ModCloth</a>, and <a href='https://www.airbus.com'>Airbus</a> launch new global digital initiatives. He is named on a number of patents focusing on mobile interactions and digital entertainment. Evan is an open-source innovator, and frequent speaker at software development conferences focusing on Product Managment, Node.JS, Rails, and databases.
+          Evan has deep expertise in building the technical side of B2C products as well as the teams required to do so. He’s helped companies like <a target='_new' href='https://www.disney.com'>Disney</a>, <a target='_new' href='https://www.taskrabbit.com'>TaskRabbit</a>, <a target='_new' href='https://www.modcloth.com'>ModCloth</a>, and <a target='_new' href='https://www.airbus.com'>Airbus</a> launch new global digital initiatives. He is named on a number of patents focusing on mobile interactions and digital entertainment. Evan is an open-source innovator, and frequent speaker at software development conferences focusing on Product Managment, Node.JS, Rails, and databases.
         </p>
 
         <p>
