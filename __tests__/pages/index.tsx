13e132aa6fcd6490ca0ae55cca4dc93b856e6e83
Evan Tahler <evan@evantahler.com>
2019-11-11 12:38:13 -0800

    Typescript

---
+++ __tests__/pages/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { render } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import IndexPage from "../../pages/index";
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = undefined;
+});
+
+describe("Introcudtion Section", () => {
+  it("renders the section", () => {
+    act(() => {
+      render(<IndexPage />, container);
+    });
+    const header = container.querySelector("h1");
+    expect(header.textContent).toBe("Evan Tahler");
+  });
+});
