d4e4a918017ceb8112994314cef6810b43207a80
Evan Tahler <evantahler@gmail.com>
2021-07-24 12:25:33 -0700

    contact padding

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,6 +3,7 @@ import ContactCards from "../components/contactCards";
 import JumboImage from "../components/jumboImage";
 import Calendly from "../components/calendly";
 import SEO from "../components/seo";
+import { Alert } from "react-bootstrap";
 
 const title = "Evan Tahler: Contact";
 
@@ -17,25 +18,29 @@ function ContactPage() {
       <JumboImage src="/images/contact.jpg" />
 
       <strong>
-        <p>Let's chat!</p>
+        <p style={{ textAlign: "center" }}>Let's chat!</p>
       </strong>
 
       <ContactCards />
 
-      <p>
-        If you are looking to hire me on a contractor basis, please reach out
-        via{" "}
-        <a href="https://www.delicioushat.com" target="_new">
-          Delicious Hat
-        </a>
-        , my Web Technology Development and Consulting firm.
-      </p>
-      <p>
-        If you are looking for help with ActionHero, please{" "}
-        <a href="https://www.actionherojs.com/community" target="_new">
-          join the ActionHero community
-        </a>
-      </p>
+      <br />
+
+      <Alert variant="info">
+        <p>
+          If you are looking to hire me on a contractor basis, please reach out
+          via{" "}
+          <a href="https://www.delicioushat.com" target="_new">
+            Delicious Hat
+          </a>
+          , my Web Technology Development and Consulting firm.
+        </p>
+        <p>
+          If you are looking for help with ActionHero, please{" "}
+          <a href="https://www.actionherojs.com/community" target="_new">
+            join the ActionHero community
+          </a>
+        </p>
+      </Alert>
 
       <hr />
       <p>Or schedule a chat with me!</p>
