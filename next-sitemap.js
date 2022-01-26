910083b9962680b7f074a8006c48140e730af056
Evan Tahler <evan.tahler@grouparoo.com>
2022-01-26 11:10:37 -0800

    don't exlude any pages from sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.evantahler.com",
   generateRobotsTxt: true,
-  exclude: ["/blog/tag/*", "/blog/page/*"],
+  // exclude: ["/blog/tag/*", "/blog/page/*"],
 };
