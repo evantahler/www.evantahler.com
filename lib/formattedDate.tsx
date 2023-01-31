a53dbf869a1712244864a6f4a6d577d7193ec296
Evan Tahler <evan@evantahler.com>
2023-01-31 10:13:35 -0800

    remove toLocaleDateString

--- lib/formattedDate.tsx
+++ lib/formattedDate.tsx
@@ -1,7 +1,14 @@
-export const formattedDate = (d: string) =>
-  new Date(d).toLocaleDateString("en-us", {
-    weekday: "long",
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
+/**
+ * This method used to use toLocaleDateString() but that caused react errors where the server hydration did not match the browser's version
+ * https://reactjs.org/docs/error-decoder.html/?invariant=425
+ *
+ * @param d date string
+ * @returns date string, but better
+ */
+export const formattedDate = (d: string) => new Date(d).toDateString();
+// new Date(d).toLocaleDateString("en-us", {
+//   weekday: "long",
+//   year: "numeric",
+//   month: "short",
+//   day: "numeric",
+// });
