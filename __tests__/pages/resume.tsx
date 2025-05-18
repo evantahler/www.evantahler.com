bc3b8f945a3ae62ac78dac36b79f708f784967ea
Evan Tahler <evan@arcade.dev>
2025-05-18 12:40:03 -0700

    tests

--- __tests__/pages/resume.tsx
+++ __tests__/pages/resume.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot, Root } from "react-dom/client";
-import { act } from "react-dom/test-utils";
+import { act } from "react";
 
 import ResumePage from "../../pages/resume";
 
--- __tests__/pages/speacking.tsx
+++ __tests__/pages/speacking.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot, Root } from "react-dom/client";
-import { act } from "react-dom/test-utils";
+import { act } from "react";
 
 import SpeakingPage from "../../pages/speaking";
 
